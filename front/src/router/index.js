e9e44e94a64202ec7be6a0e84267efcf7c687690
Xiaozhe Yao <xiaozhe@Xiaozhes-MacBook-Pro.local>
2018-03-30 15:12:27 +0800

    add hiddent files

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import Workspace from '@/components/Workspace'
+import Workspace from '@/pages/Workspace'
 
 Vue.use(Router)
 
